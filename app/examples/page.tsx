f2e92758654fbce80f2572c0890cf5acb51d8822
kuba <hladikjakubis@gmail.com>
2024-06-13 10:34:09 +0200

    primarly md change

--- app/examples/page.tsx
+++ app/examples/page.tsx
@@ -1,26 +1,27 @@
-
+import Gradient from "@/components/gradient"
 import { createClient } from "@/lib/supabase/server"
 import Link from "next/link"
-
-export default async function Page(){
+import React from 'react'
+export default async function Page() {
     const supabase = createClient()
-    const {data, error} = await supabase.from("examples_public").select("*")
-    if(error){
+    const { data, error } = await supabase.from("examples_public").select("*")
+    if (error) {
         console.error(error)
         return <div>Failed to fetch examples</div>
     }
-    if(!data){
+    if (!data) {
         return <div>Examples not found</div>
     }
     return (
-        <div className="w-full min-h-screen">
-        <div className=" grid grid-cols-4 gap-4 p-4">
-            {data.map((example) => (
-                <Link href={"examples/" + example.id} key={example.id} className="flex h-full bg-primary-foreground p-4 rounded items-center justify-center max-h-24 ">
-                    <h1>{example.name}</h1>
-                </Link>
-            ))}
-        </div>
-        </div>
+        <div className="w-full min-h-screen z-0 ">
+            <div className=" grid grid-cols-4 gap-4 p-4 bg-transparent ">
+                {data.map((example) => (
+                    <Link href={"examples/" + example.id} key={example.id} className=" z-50 flex h-full bg-primary-foreground p-4 rounded items-center justify-center max-h-24 ">
+                        <h1>{example.name}</h1>
+                    </Link>
+                ))}
+            </div>
+            <Gradient />
+        </div>  
     )
 }
