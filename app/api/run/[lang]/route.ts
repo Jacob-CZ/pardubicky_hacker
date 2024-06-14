7806028d458f39a929840b98f061f413d7d09efa
kuba <hladikjakubis@gmail.com>
2024-06-14 11:46:00 +0200

    add output history to assistant

--- app/api/run/[lang]/route.ts
+++ app/api/run/[lang]/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: NextRequest, { params }: { params: { lang: strin
   const { data: publicEampleData, error: publicError } = await supabase.from("examples_public").select("*").eq("id", exampleId).single();
   const { data: exampleData, error } = await supabase.from("examples_private").select("*").eq("exampleId", exampleId).single();
   if (error || publicError) {
-    console.error("error " ,  error, " public error" ,  publicError);
+    console.error(error, publicError);
     return NextResponse.json(error || publicError, { status: 500 });
   }
   if (!exampleData || !publicEampleData) {
@@ -41,25 +41,22 @@ export async function POST(req: NextRequest, { params }: { params: { lang: strin
     data = await res.text();
   } catch (e) {
     console.error(e)
-    return NextResponse.json("Failed to run code");
+    return NextResponse.json("Failed to run code", { status: 500 });
   }
   console.log(data)
-  if(!data.includes(secret)){
-    return NextResponse.json("Failed to test code:" + data);
-  }
   const dataToUser = data.split(secret)[0]
   let results = data.split(secret)[1]
   if (!results) {
-    return NextResponse.json("Failed to test code:" + dataToUser);
+    return NextResponse.json("Failed to test code:" + dataToUser, { status: 500 });
   }
   results = results.replace(/(undefined)/gm, "");
   const result: boolean[] = compareResults(JSON.parse(results), testCaseArray)
   for (let index = 0; index < result.length; index++) {
     if (!result[index]) {
       console.log(result[index])
-      return NextResponse.json(String(`Test cases failed expected:${testCaseArray[index].output} got:${(JSON.parse(results) as Array<any>)[index]} \n output:${dataToUser}`))
+      return NextResponse.json(String(`Test cases failed expected:${testCaseArray[index].output} got:${(JSON.parse(results) as Array<any>)[index]} output:\n` + dataToUser), { status: 400 })
     }
   }
-  return NextResponse.json(data + "all testcasese passed", { status: 200 });
+  return NextResponse.json(dataToUser + "all testcasese passed", { status: 200 });
 }
 
